057cd43339de35a379e9857a7a529ddf739331e5
Ayan Kaisabekov <ayanprog@yandex.ru>
2017-11-29 12:49:32 +0600

    commit

--- src/main/java/booking/Storage.java
+++ src/main/java/booking/Storage.java
@@ -9,5 +9,22 @@ public class Storage {
     LinkedHashMap movie = new LinkedHashMap();
 
 
-    public add User
+    public void addUser(String login){
+        this.users.put("Name","login");
+    }
+    public void addManager(String login){
+        this.manager.put("Name","login");
+    }
+    public void addMovie(String name){
+        this.movie.put("Name","name");
+    }
+    public boolean checkUser(String login){
+        return this.users.containsValue("login");
+    }
+    public boolean checkManager(String login){
+        return this.manager.containsValue("login");
+    }
+    public boolean checkMovie(String name){
+        return this.movie.containsValue("login");
+    }
 }
